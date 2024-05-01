860d71c867588a4dbdc5b605c017008c0fb1ebe1
Bitonobit <bitonobit@gmail.com>
2024-05-01 19:43:46 +0100

    Funciones matemáticas, de cadenas y lógicas

---
+++ Clases/funcionesMatematicas.py
@@ -0,0 +1,10 @@
+
+# **********************************************
+#            Funciones matemáticas
+# **********************************************
+# Python no tiene una random() función para crear 
+# un número aleatorio, pero Python tiene un módulo 
+# integrado llamado random que se puede utilizar para 
+# crear números aleatorios:
+import random
+print(random.randrange(1, 10))
