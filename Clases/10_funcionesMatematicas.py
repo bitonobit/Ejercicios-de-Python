72bac855158e2d782f154d1339218fbab5a853e8
Bitonobit <bitonobit@gmail.com>
2024-05-02 12:59:05 +0100

    Json

--- Clases/10_funcionesMatematicas.py
+++ Clases/10_funcionesMatematicas.py
@@ -7,3 +7,15 @@
 # crear números aleatorios:
 import random
 print(random.randrange(1, 10))
+import math
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+x = abs(-7.25)
+x = pow(4, 3)
+x = math.sqrt(64)
+x = math.ceil(1.4)
+y = math.floor(1.4)
+x= math.trunc(2.77)
+x = math.pi
+x = math.factorial(9)
+x = math.isnan (56)           #Este método devuelve True si el valor especificado es NaN
