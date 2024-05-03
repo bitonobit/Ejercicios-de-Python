359fae4476e99cab84b236faa5e72175416a6130
Bitonobit <bitonobit@gmail.com>
2024-05-03 13:48:19 +0100

    Mejorando la documentación del código

--- CasoPractico.py
+++ CasoPractico.py
@@ -1,37 +1,69 @@
-class Tareas:                            #Crea la clase tareas
-    def __init__(self):                  #Constructor de la clase, crea la lista
+#******************************************************************************* 
+#  Caso Práctico Final: Gestor de tareas
+#  Autora: Candy González Delgado
+#  Fecha: 03/05/2024
+#******************************************************************************* 
+
+#Crea la clase Tareas
+class Tareas:                
+    def __init__(self):      
+        #Constructor de la clase, crea la lista
+        #@Param self 
         self.tareas = []
 
-    def agregar_tarea(self, tarea):      #Método para agregar tareas a la lista con un diccionario con descripción y estado
+    def agregar_tarea(self, tarea):      
+        #Método para agregar tareas a la lista con un diccionario 
+        #con descripción y estado. 
+        #@Param self 
+        #@Param string tarea 
         self.tareas.append({'descripcion': tarea, 'completada': False})
 
-    def marcar_completada(self, posicion):  #Método para cambiar el estado de una tarea
+    def marcar_completada(self, posicion):  
+        #Método para cambiar el estado de una tarea
+        #@Param self
+        #@Param int posicion 
         try:
-            self.tareas[posicion]['completada'] = True  #Marca la tareas como completada
+            #Marca la tareas como completada
+            self.tareas[posicion]['completada'] = True  
             print("Tarea marcada como completada.")
         except IndexError:
+            #Muestra un mensaje de error si el número
+            #No está en el menú de opciones
             print("La posición ingresada no es válida.")
 
-    def mostrar_tareas(self):                        #Muestra la lista de tareas            
+    def mostrar_tareas(self):                        
+        #Muestra la lista de tareas            
         if not self.tareas:
-            print("No hay tareas pendientes.")       #Si no hay tareas
+            #Si no hay tareas muestra un mensaje
+            print("No hay tareas pendientes.")       
         else:
-            for i, tarea in enumerate(self.tareas):  #Si hay tareas, para cada tarea muestra el estado
-                estado = "Completada" if tarea['completada'] else "Pendiente"   #Si esta completada o pendiente, con el operador ternario
-                print(f"{i+1}. {tarea['descripcion']} - {estado}")  #Muestra por consola con formato la tarea y su estado
+            #Si hay tareas, para cada tarea muestra el estado
+            for i, tarea in enumerate(self.tareas):  
+                #Si esta completada o pendiente, con el operador ternario
+                estado = "Completada" if tarea['completada'] else "Pendiente"   
+                #Muestra por consola con formato la tarea y su estado
+                print(f"{i+1}. {tarea['descripcion']} - {estado}")  
 
-    def eliminar_tarea(self, posicion):     #Método para eliminar una tarea, recibe como parámetro el número de la tarea a eliminar
+    def eliminar_tarea(self, posicion):     
+        #Método para eliminar una tarea, 
+        #usando el número de la tarea a eliminar
+        #@Param self
+        #@Param int posicion 
         try:
-            del self.tareas[posicion]       #Elimina la tarea de la lista, eliminando la posición
+            #Elimina la tarea de la lista, eliminando la posición
+            del self.tareas[posicion]       
             print("Tarea eliminada correctamente.")
-        except IndexError:                  #Salta si la posición a eliminar no existe
+        except IndexError:                  
+            #Salta si la posición a eliminar no existe
             print("La posición ingresada no es válida.")
 
 
-# Función principal para ejecutar el programa
 def main():
-    lista_tareas = Tareas() #Crea una instancia de la clase Tareas en la variable lista_tareas
-    while True:             #Bucle para mostrar las opciones del menú
+# Función principal para ejecutar el programa
+    #Crea una instancia de la clase Tareas en la variable lista_tareas
+    lista_tareas = Tareas() 
+    while True:             
+        #Bucle para mostrar las opciones del menú
         print("\n--- Gestor de Tareas ---")
         print("1. Agregar nueva tarea")
         print("2. Marcar tarea como completada")
@@ -39,35 +71,57 @@ def main():
         print("4. Eliminar una tarea")
         print("5. Salir")
 
-        opcion = input("Seleccione una opción: ")   #Entrada de datos para saber que opción se quiere ejecutar
+        #Entrada de datos para escoger que opción se quiere ejecutar
+        opcion = input("Seleccione una opción: ")   
 
-        if opcion == '1':              #Si se elige la opción 1
-            nueva_tarea = input("Ingrese la nueva tarea: ")   #Permite escribir la descripción de la tarea
-            lista_tareas.agregar_tarea(nueva_tarea) #Llama al método agregar_tarea con la descripción
-        elif opcion == '2':            #Si se elige la opción 2
-            lista_tareas.mostrar_tareas()   #llama al método mostrar_tareas() para verlas todas
+        if opcion == '1':              
+            #Si se elige la opción 1
+            #Pide escribir la descripción de la tarea
+            nueva_tarea = input("Ingrese la nueva tarea: ")   
+            #Llama al método agregar_tarea con la descripción
+            lista_tareas.agregar_tarea(nueva_tarea) 
+        elif opcion == '2':            
+            #Si se elige la opción 2
+            #Llama al método mostrar_tareas()
+            lista_tareas.mostrar_tareas()   
             try:
-                posicion = int(input("Ingrese la posición de la tarea que desea marcar como completada: ")) - 1     #Pide el número de la tarea a marcar como completada y le resta 1 porque el índice de la lista comienza en 0
-                lista_tareas.marcar_completada(posicion)    #Llama al método completar con el número de la tarea
-            except ValueError:      #Salta si se escoge un número que no este en la lista
+                #Pide el número de la tarea a marcar como completada 
+                #y le resta 1 porque el índice de la lista comienza en 0
+                posicion = int(input("Ingrese la posición de la tarea que desea marcar como completada: ")) - 1     
+                #Llama al método completar con el número de la tarea
+                lista_tareas.marcar_completada(posicion)    
+            except ValueError:      
+                #Salta el mensaje si se escoge un número 
+                #que no este en la lista
                 print("Por favor ingrese un número válido.")
         elif opcion == '3':
-            lista_tareas.mostrar_tareas()   #Muestra la lista con todas las tareas
+            #Muestra la lista con todas las tareas
+            lista_tareas.mostrar_tareas()   
         elif opcion == '4':
-            lista_tareas.mostrar_tareas()   #Muestra la lista con todas las tareas
+            #Muestra la lista con todas las tareas
+            lista_tareas.mostrar_tareas()   
             try:
-                posicion = int(input("Ingrese la posición de la tarea que desea eliminar: ")) - 1   #Pide el número de la tarea a eliminar y  le resta 1 porque el índice de la lista comienza en 0
-                lista_tareas.eliminar_tarea(posicion) #invoca al método eliminar_tarea con el número escogido
-            except ValueError: #Salta si se escoge un número que no este en la lista
+                #Pide el número de la tarea a eliminar 
+                #y le resta 1 porque el índice de la lista comienza en 0
+                posicion = int(input("Ingrese la posición de la tarea que desea eliminar: ")) - 1   
+                #Invoca al método eliminar_tarea con el número escogido
+                lista_tareas.eliminar_tarea(posicion) 
+            except ValueError: 
+                #Salta el mensaje si se escoge un número que no este en la lista
                 print("Por favor ingrese un número válido.")
         elif opcion == '5':     
-            print("¡Hasta la próxima vez!") #Mensaje de despedida
-            break       #Sale del bucle while
+            #Mensaje de despedida
+            print("¡Hasta la próxima vez!") 
+            #Sale del bucle while
+            break       
         else:
-            print("Opción no válida. Por favor seleccione una opción válida.")  #Muestra este mensaje si se coloca un número que no esté en el menu de opciones
+            #Muestra este mensaje si se coloca un número que no esté en el menu de opciones
+            print("Opción no válida. Por favor seleccione una opción válida.")  
 
 
-if __name__ == "__main__":  #Pregunta si el archivo está siendo ejecutado como programa principal
-    main()  # Este código se ejecuta cuando el archivo se ejecuta directamente
+#Pregunta si el archivo está siendo ejecutado como programa principal
+if __name__ == "__main__":  
+    # Llama a la función main
+    main()  
 
 
